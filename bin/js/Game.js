0e4c566227a99f46218eb70688c5a1aa3d6ba211
bookoman <bookoman@163.com>
2018-04-05 18:34:43 +0800

    ........

---
+++ bin/js/Game.js
@@ -0,0 +1,12 @@
+/*
+* 游戏入口
+*/
+var Game = /** @class */ (function () {
+    function Game() {
+        this.init();
+    }
+    Game.prototype.init = function () {
+    };
+    return Game;
+}());
+//# sourceMappingURL=Game.js.map
