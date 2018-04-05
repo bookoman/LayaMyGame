b1b3920a640cd064c6d747d9c31a5770e2cf215f
bookoman <bookoman@163.com>
2018-04-05 18:55:04 +0800

    提交

--- bin/js/Game.js
+++ bin/js/Game.js
@@ -6,10 +6,7 @@ var Game = /** @class */ (function () {
         this.init();
     }
     Game.prototype.init = function () {
-<<<<<<< HEAD
-=======
         console.log("我是笔记本写的方法");
->>>>>>> e9394f7f1941479c8a268369f207997fe76799eb
     };
     return Game;
 }());
